7b360b087675f095dc96371c4a04bba8c6c19590
boyka <abdelrahmanzaki747@gmail.com>
2023-03-05 16:26:04 +0200

    setup files

---
+++ src/pages/messages/Messages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Messages.scss"
+function Messages() {
+  return (
+    <div>Messages</div>
+  )
+}
+
+export default Messages
---
+++ src/pages/orders/Order.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Order.scss"
+function Order() {
+  return (
+    <div>Order</div>
+  )
+}
+
+export default Order
