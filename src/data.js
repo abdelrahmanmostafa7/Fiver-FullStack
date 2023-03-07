a4f69a49637e41d2157c8e44c4649cbcdf5fbe3a
boyka <abdelrahmanzaki747@gmail.com>
2023-03-07 13:41:26 +0200

    trusted icons and slider

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const cards = [
+    {
+      id: 1,
+      title: "AI Artists",
+      desc: "Add talent to AI",
+      img: "https://images.unsplash.com/photo-1542614471-001ccf2b449c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 2,
+      title: "Logo Design",
+      desc: "Build yor brand",
+      img: "https://images.unsplash.com/photo-1542466500-dccb2789cbbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 3,
+      title: "WordPress",
+      desc: "Customize your site",
+      img: "https://images.unsplash.com/photo-1542451313056-b7c8e626645f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 4,
+      title: "Voice Over",
+      desc: "Share your message",
+      img: "https://images.unsplash.com/photo-1542466500-dccb2789cbbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 5,
+      title: "Video Explainer",
+      desc: "Engage your audience",
+      img: "https://images.pexels.com/photos/13388047/pexels-photo-13388047.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 6,
+      title: "Social Media",
+      desc: "Reach more customers",
+      img: "https://images.pexels.com/photos/11378899/pexels-photo-11378899.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 7,
+      title: "SEO",
+      desc: "Unlock growth online",
+      img: "https://images.pexels.com/photos/4820241/pexels-photo-4820241.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 8,
+      title: "Illustration",
+      desc: "Color you dreams",
+      img: "https://images.pexels.com/photos/15032623/pexels-photo-15032623.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+  
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import Slide from '../../components/slide/Slide'
+import TrustedBy from '../../components/trustedBy/TrustedBy'
 import "./Home.scss"
+import {cards} from "../../data"
+import CatCard from "../../components/catCard/CatCard"
+
+
+
 function Home() {
   return (
     <div>
       <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+      {cards.map(card=>(
+            <CatCard  item={card} key={card.id}/>
+          ))}
+      </Slide>
     </div>
   )
 }
