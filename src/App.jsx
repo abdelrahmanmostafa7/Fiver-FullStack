4f3f7bb0bc5014b3528b2c45ef354b01d9f50448
boyka <abdelrahmanzaki747@gmail.com>
2023-03-07 18:04:13 +0200

    remove login&register pages until start backend

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/home/Home";
 import Gigs from "./pages/gigs/Gigs";
 import Gig from "./pages/gig/Gig";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
 import Add from "./pages/add/Add";
 import Orders from "./pages/orders/Orders";
 import Messages from "./pages/messages/Messages";
@@ -65,14 +65,14 @@ function App() {
         },
       ],
     },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
+    // {
+    //   path: "/register",
+    //   element: <Register />,
+    // },
+    // {
+    //   path: "/login",
+    //   element: <Login />,
+    // },
   ]);
 
   return <RouterProvider router={router} />;
